b94e978dedfd098f75dd967ce488835bb9a03cc7
tcotidiane <51900661+tcotidiane33@users.noreply.github.com>
2022-02-18 10:58:14 +0100

    update lib

--- manage/src/App.js
+++ manage/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Files from "./components/Files";
+import Taches from "./components/Taches";
+import Ressource from "./components/Ressource";
+import Tool from "./components/Tool";
+import Settings from "./components/Settings";
+import Help from "./components/Help";
+import NotFound from "./pages/NotFound";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/files" component={Files} />
+          <Route path="/taches" component={Taches} />
+          <Route path="/ressource" component={Ressource} />
+          <Route path="/tool" component={Tool} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/help" component={Help} />
+        
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
